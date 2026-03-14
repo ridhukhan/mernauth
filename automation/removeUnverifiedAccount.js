2c112f6e6a00b7f90c5fd20e8ae2a9eccec876f6
ridhukhan <prakritirshandhane@gmail.com>
2026-03-14 20:29:54 +0600

    update

--- automation/removeUnverifiedAccount.js
+++ automation/removeUnverifiedAccount.js
@@ -1,13 +1,13 @@
 import cron from "node-cron"
 import { User } from "../models/userModel.js"
-export const removeUnverifiedAccount = ()=>{
-cron.schedule("*/30 * * * * ",async()=>{
 
-
-    const thertyMinuteAgo= new Date(Date.now() - 30 * 60 * 1000);
-     await User.deleteMany({
-        accountVerified:false,
-        createdAt:{$lt: thertyMinuteAgo}
+export const removeUnverifiedAccount = () => {
+  cron.schedule("*/30 * * * *", async () => {  // ✅ space বাদ দিলাম
+    const thirtyMinutesAgo = new Date(Date.now() - 30 * 60 * 1000);
+    await User.deleteMany({
+      accountVerified: false,
+      createdAt: { $lt: thirtyMinutesAgo }
     })
-})
+    console.log("Unverified accounts removed")
+  })
 }
